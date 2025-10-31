11762493d7915f0fc5b71c6293149ed354ce2304
Aidan <aidanr@databean.com>
2025-10-31 14:33:11 +0100

    added shell script for easier connection

---
+++ connect.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# 1. Open a Terminal window to run redis-server
+osascript <<EOF
+tell application "Terminal"
+    activate
+    do script "cd '$(pwd)'; echo 'Starting redis-server...'; redis-server conf/redis/redis.conf; exec bash"
+end tell
+EOF
+
+# 2. Open another Terminal window to activate the environment and run Dialogflow CX
+osascript <<EOF
+tell application "Terminal"
+    activate
+    do script "cd '$(pwd)'; source .venv/bin/activate; echo 'Environment activated'; run-dialogflow-cx; exec bash"
+end tell
+EOF
+
+# 2. Open another Terminal window to run python scripts
+osascript <<EOF
+tell application "Terminal"
+    activate
+    do script "cd '$(pwd)'; source .venv/bin/activate; echo 'Run python scripts in this terminal'"
+end tell
+EOF
+
+chmod +x connect.sh
